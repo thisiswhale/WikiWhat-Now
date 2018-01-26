52a88a7b4b801348def06d765ae9a482b289775f
William Leung <leungwk25@gmail.com>
2018-01-25 23:40:57 -0800

    refractoring and rendered data to html

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
+const list =  document.querySelector('#render-box');
+const error = document.querySelector('#error-box');
+
+document.addEventListener('keypress', event => {
+  var key = event.which || event.keyCode;
+    if (key === 13) { // 13 is enter
+      displayWikiResults()
+    }
+});
 
 function displayWikiResults() {
+  reset();
   const listOfData = [];
   let query = document.querySelector('#search-box').value;
-  let url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts&generator=prefixsearch&exchars=700&exlimit=10&exintro=1&explaintext=1&gpsnamespace=0&gpslimit=10&gpssearch=pokemon" //+query;
+  let url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts&generator=prefixsearch&exchars=700&exlimit=10&exintro=1&explaintext=1&gpsnamespace=0&gpslimit=10&gpssearch=" +query;
   fetch(url).then(response => response.json()).then(data => {
-    let hey = data.query.pages
-    console.log(data.query.pages)
-    for(let key in hey){
+    let realData = data.query.pages;
+    for (let key in realData) {
+      listOfData.push({pageid: realData[key].pageid, title: realData[key].title, extract: realData[key].extract});
+    }
+    renderResults(listOfData);
+  }).catch(err => {
+    error.innerHTML = `<p>Error: Type something!!!</p>`;console.log(err)});
 
-     listOfData.push({pageid: hey[key].pageid,
-                      title: hey[key].title,
-                      extract: hey[key].extract })
+}
+function reset(){
+  list.innerHTML = "";
+  error.innerHTML = "";
 
-   }
-    // return listOfData.map(result => {
-    //   let title = document.createElement('h3');
-    //   title.setAttribute('class', 'result-title');
-    //   title.setAttribute('data-key', `${result.pageid}`);
-    //   title.innerHTML = `${result.title}`;
-    //   append(title);
-    // });
-    //  `<div class='result-list'>
-    //      <h3 class='result-title' data-key="${result.pageid}">${result.title}</h3>
-    //      <p class='result-extract'>${result.extract}</p>
-    // </div>`).join('');
-  }).catch(err => console.log(err));
-  console.log(listOfData);
+}
+function renderResults(data){
+  const dataRender =  data.map(result => `
+      <div class='result-box'>
+         <h3 class='result-title' data-key="${result.pageid}">${result.title}</h3>
+         <p class='result-extract'>${result.extract}</p>
+    </div>`).join('');
+    list.innerHTML = dataRender;
 }
 
-//this method works
-// for(let key in listOfData){
-//   let title = document.createElement('h3');
-//   title.setAttribute('class', 'result-title');
-//   title.setAttribute('data-key',listOfData[key].pageid);
-//   title.innerHTML = listOfData[key].title;
+
+//this method works also
 //
-//   document.querySelector("#result-box").appendChild(title)
-//   //listOfData[key].pageid
-//   //listOfData[key].title
-//   //listOfData[key].extract
-// }
+// function displayWikiResults() {
+//
+//   let query = document.querySelector('#search-box').value;
+//   let url = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts&generator=prefixsearch&exchars=700&exlimit=10&exintro=1&explaintext=1&gpsnamespace=0&gpslimit=10&gpssearch=pokemon" //+query;
+//   fetch(url).then(response => response.json()).then(data => {
+//     let listOfData = data.query.pages;
+//     for(let key in listOfData){
+//       let title = document.createElement('h3');
+//       title.setAttribute('class', 'result-title');
+//       title.setAttribute('data-key',listOfData[key].pageid);
+//       title.innerHTML = listOfData[key].title;
+//
+//       document.querySelector("#result-box").appendChild(title);
+//       // listOfData[key].pageid
+//       // listOfData[key].title
+//       // listOfData[key].extract
+//     }
+//   }).catch(err => console.log(err));
+//}
